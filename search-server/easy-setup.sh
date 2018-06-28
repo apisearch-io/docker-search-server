ed976f5f23a799cbda230f8322799ca5e20af1cf
Marc Morera <yuhu@mmoreram.com>
2018-06-28 17:59:19 +0200

    Fixed very dolent error

--- search-server/easy-setup.sh
+++ search-server/easy-setup.sh
@@ -22,4 +22,4 @@ cd /var/www/apisearch && \
     appId=$(< /dev/urandom tr -dc a-z0-9 | head -c${1:-12}) && \
     indexId=$(< /dev/urandom tr -dc a-z0-9 | head -c${1:-12}) && \
     php bin/console apisearch-server:create-index $appId $indexId && \
-    php bin/console apisearch-server:generate-basic-tokens $indexId
+    php bin/console apisearch-server:generate-basic-tokens $appId
